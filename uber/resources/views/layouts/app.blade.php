3d03f9506a086af0e585fd4656dbabdf25d0bbf8
sftss <tasdemirsefer2005@gmail.com>
2024-11-25 14:33:26 +0000

    main page + layouts

---
+++ uber/resources/views/layouts/app.blade.php
@@ -0,0 +1,35 @@
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>@yield('title')</title>
+
+
+        <link rel="stylesheet" type="text/css" href="{{asset('css/beer.css')}}"/>   
+
+    </head>
+
+
+    <body>
+
+    	<header>
+    		<h1>@yield('title')</h1>
+    	</header>
+
+        @section('nav')
+            <ul>
+                <li>Accueil</a></li>
+                <li>Les pizzas</a></li>
+            </ul>
+        @show
+
+        <div class="container">
+            @yield('content')
+        </div>
+
+
+
+    </body>
+</html>
