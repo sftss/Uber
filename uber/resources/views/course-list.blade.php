5506c0b754b717898920dad56a5054c5517b61ac
sftss <tasdemirsefer2005@gmail.com>
2024-11-27 21:21:04 +0000

    color vert + pretier

--- uber/resources/views/course-list.blade.php
+++ uber/resources/views/course-list.blade.php
@@ -5,10 +5,10 @@
 @section('title', 'Uber course')
 
 @section('nav')
-<ul>
-    <li><a href="{{ route('restaurants.index') }}">Restaurants</a></li>
-    <li><a href="{{ route('clients.index') }}">Clients</a></li>
-</ul>
+    <ul>
+        <li><a href="{{ route('restaurants.index') }}">Restaurants</a></li>
+        <li><a href="{{ route('clients.index') }}">Clients</a></li>
+    </ul>
 @show
 
 @section('sidebar')
@@ -17,13 +17,13 @@
 @endsection
 
 @section('content')
-<h2>Les courses</h2>
-<ul>
-   @foreach ($courses as $course)
-       <li>{{ $course->temps_arrivee }}</li>
-  @endforeach
-</ul>
+    <h2>Les courses</h2>
+    <ul>
+        @foreach ($courses as $course)
+            <li>{{ $course->temps_arrivee }}</li>
+        @endforeach
+    </ul>
 @endsection
-<a href="{{ url("/") }}">
+<a href="{{ url('/') }}">
     <p>arrière</p>
 </a>
